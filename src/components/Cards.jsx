fea1a1c83b3499324c0fa2ed3b37f6de8176cab5
shahinexy <shahinexy@gmail.com>
2024-04-09 14:50:47 +0600

    single card details added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,40 +1,59 @@
-import PropTypes from 'prop-types'; 
+import PropTypes from "prop-types";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Cards = ({data}) => {
-    const {id, image, estate_title, segment_name, price, description, status, location} = data;
-    return (
-        <div>
-            <div className="bg-primary text-white shadow-md shadow-black">
-                <div className="relative">
-                    <img className="w-full h-56 shadow-md shadow-secondary" src={image} alt="" />
-                    <div className="absolute top-2 right-1 flex gap-5 text-white">
-                        {status.sell && <small className="bg-primary px-2">Sell</small>}
-                        {status.rent && <small className="bg-secondary px-2">Rent</small>}
-                    </div>
-                </div>
-                <div className="md:p-4 p-2 space-y-3">
-                <p className="text-xl font-semibold">{estate_title}</p>
-                <div className="flex justify-between flex-wrap">
-                    <p>Price: {price.sell ? price.sell : price.rent}$</p>
-                    <small className="bg-secondary px-2 text-primary font-bold">{segment_name}</small>
-                </div>
-                <p className="border-b border-secondary pb-4">{description}</p>
-                <div className='flex md:flex-row flex-col-reverse justify-between md:items-center flex-wrap'>
-                <Link to={`/singleCard/${id}`}>
-                <button className="btn bg-secondary border-primary hover:bg-primary text-primary hover:text-white font-semibold rounded-none">View Details</button>
-                </Link>
-                <p className='flex gap-1 items-center md:my-0 my-3'><FaMapMarkerAlt></FaMapMarkerAlt> {location}</p>
-                </div>
-                </div>
-            </div>
+const Cards = ({ data }) => {
+  const {
+    id,
+    image,
+    estate_title,
+    segment_name,
+    price,
+    description,
+    status,
+    location,
+  } = data;
+  return (
+    <div>
+      <div className="bg-primary text-white shadow-md shadow-black">
+        <div className="relative">
+          <img
+            className="w-full h-56 shadow-md shadow-secondary"
+            src={image}
+            alt=""
+          />
+          <div className="absolute top-2 right-1 flex gap-5 text-white">
+            {status.sell && <small className="bg-primary px-2">Sell</small>}
+            {status.rent && <small className="bg-secondary px-2">Rent</small>}
+          </div>
         </div>
-    );
+        <div className="md:p-4 p-2 space-y-3">
+          <p className="text-xl font-semibold">{estate_title}</p>
+          <div className="flex justify-between flex-wrap">
+            <p>Price: {price.sell ? price.sell : price.rent}$</p>
+            <small className="bg-secondary px-2 text-primary font-bold">
+              {segment_name}
+            </small>
+          </div>
+          <p className="border-b border-secondary pb-4">{description}</p>
+          <div className="flex md:flex-row flex-col-reverse justify-between md:items-center flex-wrap">
+            <Link to={`/singleCard/${id}`}>
+              <button className="btn bg-secondary border-primary hover:bg-primary text-primary hover:text-white font-semibold rounded-none">
+                View Details
+              </button>
+            </Link>
+            <p className="flex gap-1 items-center md:my-0 my-3">
+              <FaMapMarkerAlt></FaMapMarkerAlt> {location}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Cards.propTypes = {
-    data: PropTypes.object.isRequired
-}
+  data: PropTypes.object.isRequired,
+};
 
 export default Cards;
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,25 +1,93 @@
-import { useLoaderData, useParams } from 'react-router-dom';
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaLandmark } from "react-icons/fa6";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const SingleCard = () => {
-    const datas = useLoaderData()
-    const param = useParams()
-    const data = datas.find(data => data.id === param.id)
-    const {image, estate_title, segment_name, price, description, status, area_sqft, location, facilities, rant_package_facilities, about} = data;
-    return (
+  const datas = useLoaderData();
+  const param = useParams();
+  const data = datas.find((data) => data.id === param.id);
+  const {
+    image,
+    estate_title,
+    segment_name,
+    price,
+    description,
+    status,
+    area_sqft,
+    location,
+    facilities,
+    rant_package_facilities,
+    about,
+  } = data;
+  return (
+    <div>
+      <div className="max-w-5xl mx-auto p-4">
         <div>
-            <div className='max-w-5xl mx-auto'>
-                <div>
-                    <img className='w-full h-[50vh]' src={image} alt="" />
-                </div>
-                <div className='md:px-5 px-3 my-3'>
-                <div className='flex justify-between flex-wrap'>
-                    <p className='text-3xl font-bold'>{estate_title}</p>
-                    <p className='bg-primary px-10 text-white'>{segment_name}</p>
-                </div>
-                </div>
-            </div>
+          <img className="w-full h-[50vh]" src={image} alt="" />
         </div>
-    );
+        <div className="my-3">
+          <div className="flex justify-between items-center flex-wrap gap-2">
+            <p className="text-3xl font-bold">{estate_title}</p>
+            <p className="bg-primary px-3 text-white ">{segment_name}</p>
+          </div>
+          <p className="mt-3">{description}</p>
+          <div className="flex gap-5 flex-wrap my-3">
+            <p className="flex gap-2 items-center">
+              <FaLandmark></FaLandmark>
+              {area_sqft} sq ft{" "}
+            </p>
+            <p className="flex gap-2 items-center">
+              <FaMapMarkerAlt></FaMapMarkerAlt> {location}{" "}
+            </p>
+            {status.sell && <p className="bg-primary px-2 text-white">Sell</p>}
+            {status.rent && (
+              <p className="bg-secondary px-2 text-primary">Rent</p>
+            )}
+          </div>
+          <div className="grid md:grid-cols-2 grid-cols-1 my-5">
+            <div>
+              <h2 className="text-center text-xl font-bold bg-secondary py-3 text-primary">
+                Fore Sell
+              </h2>
+              <div className="p-3 space-y-3">
+                <p className="font-semibold">Facilities</p>
+                <ul className="list-disc pl-8">
+                  {facilities.map((data, idx) => (
+                    <li key={idx}>{data}</li>
+                  ))}
+                </ul>
+                <p className="font-bold text-primary text-lg">
+                  Price: {price.sell}$
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-center text-xl font-bold bg-primary py-3 text-white">
+                Fore Rent
+              </h2>
+              <div className="p-3 space-y-3">
+                <p className="font-semibold">Facilities</p>
+                <ul className="list-disc pl-8">
+                  {rant_package_facilities.map((data, idx) => (
+                    <li key={idx}>{data}</li>
+                  ))}
+                </ul>
+                <p className="font-bold text-secondary text-lg">
+                  Price: {price.rent}$
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold text-primary pt-3 border-t border-secondary">
+              About the property
+            </h2>
+            <p>{about}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleCard;
